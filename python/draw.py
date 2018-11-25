d11fc30ecf3305760dd4fd8adc00a4172fd1eeae
cube <bedrook@gmail.com>
2018-11-26 04:02:28 +0900

    샘플 코드 추가

--- python/draw.py
+++ python/draw.py
@@ -1,10 +1,15 @@
 import tkinter
 import math
-
 # 벡터를 이용한 도형그리기
 
+def getVec(x1, y1, x2, y2, l, o):
+    
+    x2_x1 = x2-x1
+    y1_y2 = y1-y2
+
+    return ((l * y1_y2) / math.sqrt(y1_y2**2 + x2_x1**2))*o, ((l * x2_x1) / math.sqrt(y1_y2**2 + x2_x1**2))*o
+
 def get_pos(x1, y1, x2, y2, l, o):
-    rx, ry = 0, 0
     
     x2_x1 = x2-x1
     y1_y2 = y1-y2
@@ -14,19 +19,33 @@ def get_pos(x1, y1, x2, y2, l, o):
 
     return rx, ry
 
+
+l = 50
 x1, y1, x2, y2 = 100, 100, 200, 200
 window = tkinter.Tk()
 canva = tkinter.Canvas(window)
 
-x3, y3 = get_pos(x1, y1, x2, y2, 50, 1)
+x3, y3 = get_pos(x1, y1, x2, y2, l, -1)
 canva.create_line(x2, y2, x3, y3)
 canva.create_line(x1, y1, x3, y3)
 
-x3, y3 = get_pos(x1, y1, x2, y2, 50, -1)
+x3, y3 = get_pos(x1, y1, x2, y2, l, 1)
+print(x1, y1, x2, y2, x3, y3)
 canva.create_line(x2, y2, x3, y3)
 canva.create_line(x1, y1, x3, y3)
 
-line = canva.create_line(x1, y1, x2, y2)
+
+dxv, dyv = getVec(x1, y1, x2, y2, l//3, 1)
+
+dx1, dy1 = x1 + dxv, y1 + dyv
+dx2, dy2 = x2 + dxv, y2 + dyv
+dx3, dy3 = x3 + dxv, y3 + dyv #get_pos(dx1, dy1, dx2, dy2, l, 1)
+
+print(dx1, dy1, dx2, dy2, dx3, dy3)
+canva.create_line(dx2, dy2, dx3, dy3)
+canva.create_line(dx1, dy1, dx3, dy3)
+
+# line = canva.create_line(x1, y1, x2, y2)
 
 canva.pack()
 
