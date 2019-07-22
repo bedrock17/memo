8a35ca579376c39c65f19abe2562e5024054d7a0
SHK <bedrook@focushns.com>
2019-07-22 11:35:55 +0900

    datetime

---
+++ python/datetime.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime, timedelta
+
+startTime = datetime(2019, 7, 2, 0, 0, 0)
+endTime = datetime(2022, 5, 1, 0, 0, 0)
+
+totTime = endTime - startTime
+totTimeStamp = totTime.total_seconds()
+
+print(startTime, endTime, datetime.now())
+
+while True:
+  now = datetime.now()
+  leftTime = endTime - now
+
+  nowTimeStamp = (now - startTime).total_seconds()
+
+  print(leftTime, "%f%%"%(nowTimeStamp / totTimeStamp * 100))
+  time.sleep(0.3)
