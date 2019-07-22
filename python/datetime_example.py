199cf2605f1ff3bfc082ea102c4a2f1b2a2c6c8b
SHK <bedrook@focushns.com>
2019-07-22 11:48:54 +0900

    datetime update

--- python/datetime_example.py
+++ python/datetime_example.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime, timedelta
 
+
 startTime = datetime(2019, 7, 2, 0, 0, 0)
 endTime = datetime(2022, 5, 1, 0, 0, 0)
 
@@ -9,11 +10,12 @@ totTimeStamp = totTime.total_seconds()
 
 print(startTime, endTime, datetime.now())
 
+
 while True:
   now = datetime.now()
   leftTime = endTime - now
 
   nowTimeStamp = (now - startTime).total_seconds()
 
-  print(leftTime, "%f%%"%(nowTimeStamp / totTimeStamp * 100))
+  print(leftTime, "(%d weeks)"%(leftTime.total_seconds() / (60 * 60 * 24 * 7)) , "%f%%"%(nowTimeStamp / totTimeStamp * 100))
   time.sleep(0.3)
