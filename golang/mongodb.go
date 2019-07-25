603822e5ba5173c08983ab0904c05a2df58ffe5a
SHK <bedrook@focushns.com>
2019-07-25 10:10:23 +0900

    sample code update

--- golang/mongodb.go
+++ golang/mongodb.go
@@ -11,11 +11,20 @@ import (
 
 //동작하는 mongodb 샘플코드
 
-// Trainer You will be using this Trainer type later in the program
+//Phone : 임베딩된 구조체 테스트
+type Phone struct {
+	Os     string
+	Name   string
+	Number string
+}
+
+//Person : DB에 넣은 샘플 포맷
 type Person struct {
-	Name string
-	Age  int
-	City string
+	Name      string
+	Age       int
+	City      string
+	Tmp       int64
+	CellPhone Phone
 }
 
 func main() {
@@ -34,15 +43,17 @@ func main() {
 	}
 
 	fmt.Println("Connected to MongoDB!")
+
 	collection := client.Database("golangTest").Collection("golang")
 
-	ruan := Person{"Ruan", 34, "Cape Town"}
-	james := Person{"James", 32, "Nairobi"}
-	frankie := Person{"Frankie", 31, "Nairobi"}
+	ruan := Person{Name: "Ruan", Age: 34, City: "Cape Town", Tmp: 10, CellPhone: Phone{Os: "IOS", Name: "S1", Number: "123-123-123"}}
+	james := Person{Name: "James", Age: 32, City: "Nairobi", Tmp: 20, CellPhone: Phone{Os: "Android", Name: "S2", Number: "123-356-123"}}
+	frankie := Person{Name: "Frankie", Age: 31, City: "Nairobi", Tmp: 30, CellPhone: Phone{Os: "Android", Name: "S3", Number: "123-222-123"}}
 
 	trainers := []interface{}{ruan, james, frankie}
 
 	insertManyResult, err := collection.InsertMany(context.TODO(), trainers)
+
 	if err != nil {
 		log.Fatal(err)
 	}
